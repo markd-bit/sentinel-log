0af9cbc4b5917ca14691ddfff9274e1464c86014
markd-bit <61188756+markd-bit@users.noreply.github.com>
2024-11-29 16:28:34 +0000

    Delete usg-scripts directory

--- usg-scripts/update-block-list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/vbash
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-while read p; do
-	/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set firewall group address-group testgroup1 address $p
-done <IPList.txt
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
